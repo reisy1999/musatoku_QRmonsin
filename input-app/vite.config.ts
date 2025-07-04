bbc241668dfac80562c09801f758c68003fa5fb1
reisy1999 <abereneibu@gmail.com>
2025-07-04 18:06:02 +0900

    https->http

--- input-app/vite.config.ts
+++ input-app/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
-    https: true,
     port: 5174,
   }
 })
