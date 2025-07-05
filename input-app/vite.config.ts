6561889f7466767ce1ef7156b030fc613c7c1207
reisy1999 <abereneibu@gmail.com>
2025-07-05 12:27:16 +0900

    refactor: add azure mock api plugin

--- input-app/vite.config.ts
+++ input-app/vite.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const mockApiPlugin = () => ({
-  name: 'mock-api',
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
-      if (req.url?.startsWith('/api/templates/')) {
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({ template: { id: 1, name: 'Sample' } }))
-        return
-      }
-      next()
-    })
-  },
-})
+import mockAzureApiPlugin from './mock/azure/mockApiPlugin'
 
 export default defineConfig({
-  plugins: [react(), mockApiPlugin()],
+  plugins: [react(), mockAzureApiPlugin()],
   server: {
     port: 5174,
   },
